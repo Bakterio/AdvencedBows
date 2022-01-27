86712f5ebdf361f1884d7921ef65c115dfa5f1f0
bakterio <lukastoman594@gmail.com>
2022-01-27 20:06:57 +0100

    teleport bow listener

--- src/main/java/cz/bakterio/spigot/advencedbows/AdvencedBows.java
+++ src/main/java/cz/bakterio/spigot/advencedbows/AdvencedBows.java
@@ -2,6 +2,7 @@ package cz.bakterio.spigot.advencedbows;
 
 import cz.bakterio.spigot.advencedbows.commands.CommandsManager;
 import cz.bakterio.spigot.advencedbows.items.ItemsManager;
+import cz.bakterio.spigot.advencedbows.listeners.TeleportBowListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AdvencedBows extends JavaPlugin {
@@ -14,6 +15,8 @@ public final class AdvencedBows extends JavaPlugin {
         plugin = this;
         getPlugin().getCommand("bows").setExecutor(new CommandsManager());
         ItemsManager.manage();
+
+        getServer().getPluginManager().registerEvents(new TeleportBowListener(), this);
     }
 
     @Override
---
+++ src/main/java/cz/bakterio/spigot/advencedbows/listeners/TeleportBowListener.java
@@ -0,0 +1,20 @@
+package cz.bakterio.spigot.advencedbows.listeners;
+
+import cz.bakterio.spigot.advencedbows.items.TeleportBow;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+
+public class TeleportBowListener implements Listener {
+    @EventHandler
+    public void onBowLanded(ProjectileHitEvent e) {
+        if (!(e.getEntity().getShooter() instanceof Player p)) return;
+        if (p.getInventory().getItemInMainHand().equals(new TeleportBow().getItem())) {
+            Location loc = e.getHitBlock().getLocation();
+            p.teleport(new Location(p.getWorld(), loc.getX(), loc.getY() + 1, loc.getZ()));
+            p.sendMessage("woosh");
+        }
+    }
+}
