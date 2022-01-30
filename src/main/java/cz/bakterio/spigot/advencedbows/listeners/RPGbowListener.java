fb425a72d5771b6f119bd491983a2a6ddd246998
bakterio <lukastoman594@gmail.com>
2022-01-30 10:42:27 +0100

    RPG bow 'finished'

--- src/main/java/cz/bakterio/spigot/advencedbows/listeners/RPGbowListener.java
+++ src/main/java/cz/bakterio/spigot/advencedbows/listeners/RPGbowListener.java
@@ -1,12 +1,15 @@
 package cz.bakterio.spigot.advencedbows.listeners;
 
-import cz.bakterio.spigot.advencedbows.items.RPGbow;
+import cz.bakterio.spigot.advencedbows.items.*;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class RPGbowListener implements Listener {
     @EventHandler
@@ -17,6 +20,24 @@ public class RPGbowListener implements Listener {
             Entity tnt = p.getWorld().spawn(e.getHitBlock().getLocation(), TNTPrimed.class);
             ((TNTPrimed)tnt).setFuseTicks(0);
             e.getEntity().remove();
+        }
+    }
+
+    @EventHandler
+    public void onGrenadeShoot(ProjectileLaunchEvent e) {
+        if (!(e.getEntity().getShooter() instanceof Player p)) return;
+        if (p.getInventory().getItemInMainHand().equals(new RPGbow().getItem())) {
+            int index = ItemsManager.isItemInInventory(p, new GrenadeItem());
+            if (index != -1) {
+                Inventory inv = p.getInventory();
+                ItemStack b = inv.getItem(index);
+                b.setAmount(b.getAmount() - 1);
+                inv.setItem(index, b);
+                e.getEntity().setBounce(false);
+            } else {
+                p.sendMessage("No grenades in inventory...");
+                e.setCancelled(true);
+            }
         }
     }
 }
